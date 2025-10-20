4a38d59e4345b5b4b92e71ecd59b424d3c075a14
Nic Bachas <Nic.Bachas@kmart.com.au>
2025-10-20 17:38:47 +1100

    fix: Cloudflare workers build errors6

--- open-next.config copy.ts
+++
@@ -1,9 +0,0 @@
-import type { Config } from "@opennextjs/cloudflare";
-
-const config: Config = {
-  mode: "pages",
-  nextConfigDir: ".",
-  default: { runtime: "edge" },   // important for CF
-  // leave other options at defaults unless you know you need them
-};
-export default config;
